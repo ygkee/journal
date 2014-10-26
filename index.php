acf32515237df2aa762a94edcf87380cebec5c3d
kee <kee.yooog@gmail.com>
2014-10-26 17:38:47 +0800

    fwrite fclose
    Summary: Summary
    
    Test Plan: Test Plan
    
    Subscribers: kee
    
    Differential Revision: http://115.28.35.38:8080/D1

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
     phpinfo();
     $f = fopen();
+    fwrite($f, "\n");
+    fclose($f);
+    array_slice();
+    array_splice();
+    ob_start();
+    ob_flush();
+    ob_get_contents();
+    substr();
+    implode();
+    explode();
+
+    json_encode();
+    urlencode();
+
